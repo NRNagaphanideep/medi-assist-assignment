9a08b6bc8435eb74cf96424ad9e0cc22fb3c93b6
Nagaphanideep <95171605+NRNagaphanideep@users.noreply.github.com>
2024-03-26 17:05:40 +0530

    Add files via upload

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,1 +1,24 @@
+import React from "react";
+import "./index.css";
 
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <i class="bi bi-boxes"></i>
+        <h1 className="header-name">Medi Assist</h1>
+      </div>
+      <div className="header-options">
+        <a href="#home">Home</a>
+        <a href="#whyUs">Why Us</a>
+        <a href="#learn">Learn</a>
+        <a href="#jobs">Jobs</a>
+      </div>
+      <div className="header-right">
+        <button className="header-login">Login</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
