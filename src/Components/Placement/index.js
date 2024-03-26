73224b554024e5914054607e40421bb3a1561b6d
Nagaphanideep <95171605+NRNagaphanideep@users.noreply.github.com>
2024-03-26 17:07:58 +0530

    Add files via upload

--- src/Components/Placement/index.js
+++ src/Components/Placement/index.js
@@ -1,1 +1,30 @@
+import React from "react";
+import "./index.css";
 
+const Placement = () => {
+  return (
+    <div id="jobs" className="placement-container">
+      <h1>100% Placement Assistance</h1>
+      <div>
+        <h2>Resume Building</h2>
+        <p>
+          This resume template provides a structured format to showcase your
+          education, professional experience, skills, certifications, volunteer
+          work
+        </p>
+        <h2>Career Counselling</h2>
+        <p>
+          Make sure to customize it according to your own background,
+          qualifications, and the specific job you're applying for.
+        </p>
+        <h2>Jobs</h2>
+        <p>
+          Any other relevant information you want to include, such as language
+          proficiency, hobbies, or interests
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Placement;
